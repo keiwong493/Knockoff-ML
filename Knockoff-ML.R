fc265a4391aee2b16dd134995be1c7da4565ecf3
keiwong493 <136576586+keiwong493@users.noreply.github.com>
2025-07-23 17:50:23 +0800

    Knockoff-ML.R

--- Knockoff-ML.R
+++ Knockoff-ML.R
@@ -238,8 +238,6 @@ Get_select_info<-function(Feature_name,T_0,T_K,M=5,fdr=0.1){
   thr.w<-MK.threshold.byStat(out$kappatau[,1],out$kappatau[,2],M=M,fdr=fdr)
   highlight<-which(out$w>=thr.w)
   output<-data.frame(feature=Feature_name,
-                     kappa=out$kappatau[,1],
-                     tau=out$kappatau[,2],
                      w=as.vector(out$w),
                      q=as.vector(out$q),
                      threshold.w=thr.w,
